c657c9bcb22da8a0554e7ed7d47931809da83d0c
peppelinux <giuseppe.demarco@unical.it>
2020-09-27 00:54:51 +0200

    v0.7.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 DESC = "WebRTC python unit tests inspired to '80s"
 
 setup(name='videodrone',
-      version='0.7.6',
+      version='0.7.7',
       description=DESC,
       long_description=readme(),
       long_description_content_type='text/markdown',
