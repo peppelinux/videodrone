b03143a5c567741d22d59d39e64da7399bd5f970
peppelinux <giuseppe.demarco@unical.it>
2020-11-13 09:07:11 +0100

    pexip suffix patch

--- src/videodrone/drones/pexip_garr_chrome.py
+++ src/videodrone/drones/pexip_garr_chrome.py
@@ -14,12 +14,13 @@ warnings.filterwarnings("ignore", category=UserWarning)
 def run(room='videodrone', y4m='./y4m', lifetime=360, 
         headless=1, pin=None, **kwargs):
     
-    suffix = kwargs.get('id', random.randrange(1000))
+    num = kwargs.get('id', random.randrange(1000))
+    suffix = kwargs.get('suffix', 'default')
     browser = get_chrome_browser(y4m=y4m, headless=headless)
     browser.get(f'{URL}/{room}')
     
     time.sleep(3)
-    browser.find_element_by_id('display-name-dialog-input').send_keys(f'videodrone-{suffix}')
+    browser.find_element_by_id('display-name-dialog-input').send_keys(f'videodrone-{suffix}-{num}')
     time.sleep(1)
     browser.find_element_by_id('display-name-dialog-ok').click()
     
